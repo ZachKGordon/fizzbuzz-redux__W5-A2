a33c5edf366bbf6e812e45664c210ecdf91f3841
Zachary Gordon <z.gordon.h@gmail.com>
2015-09-30 00:17:19 -0700

    Populating (wip)

--- app.js
+++ app.js
@@ -1,33 +1,36 @@
 
-var assignment = (function () {
-  // var a = document.getElementById("small-num");
-  // var z = document.getElementById("big-num");
-  var a = 1;
-  var z = 5;
-  alert("you picked " + a + " and " + z +" !");
-  for ( a ; a <= z; a ++) {
-   if (a % 5 === 0 && a % 3 === 0) {
-    var newEl = document.createElement("p");
-    var newtext = document.createTextNode("FizzBuzz");
-    newEl.appendChild(newtext);
-    document.getElementById("output").appendChild(newEl);
-  } else if ( a % 5 === 0) {
-    var newEl = document.createElement("p");
-    var newtext = document.createTextNode("Buzz");
-    newEl.appendChild(newtext);
-    document.getElementById("output").appendChild(newEl);
-  } else if (a % 3 === 0) {
-    var newEl = document.createElement("p");
-    var newtext = document.createTextNode("Fizz");
-    newEl.appendChild(newtext);
-    document.getElementById("output").appendChild(newEl);
-  } else {
-    var newEl = document.createElement("p");
-    var newtext = document.createTextNode(a);
-    newEl.appendChild(newtext);
-    document.getElementById("output").appendChild(newEl);
+var assignment = function () {
+
+  var aEl = document.getElementById("small-num");
+  var zEl = document.getElementById("big-num");
+  var aVal = aEl.value
+  var zVal = zEl.value
+  alert("you picked " + aVal + " and " + zVal +" !");
+  for ( aVal ; aVal <= zVal; aVal ++) {
+    if (aVal % 5 === 0 && aVal % 3 === 0) {
+      var newEl = document.createElement("p");
+      var newtext = document.createTextNode("FizzBuzz");
+      newEl.appendChild(newtext);
+      document.getElementById("output").appendChild(newEl);
+    } else if ( aVal % 5 === 0) {
+      var newEl = document.createElement("p");
+      var newtext = document.createTextNode("Buzz");
+      newEl.appendChild(newtext);
+      document.getElementById("output").appendChild(newEl);
+    } else if (aVal % 3 === 0) {
+      var newEl = document.createElement("p");
+      var newtext = document.createTextNode("Fizz");
+      newEl.appendChild(newtext);
+      document.getElementById("output").appendChild(newEl);
+    } else {
+      var newEl = document.createElement("p");
+      var newtext = document.createTextNode(aVal);
+      newEl.appendChild(newtext);
+      document.getElementById("output").appendChild(newEl);
+    }
   }
-}
-}());
+};
+
+
 
 
