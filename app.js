201382a71ab096e82f0300a7ec353020c2f89393
Zachary Gordon <z.gordon.h@gmail.com>
2015-09-30 11:43:13 -0700

    Add in object and break up logic

--- app.js
+++ app.js
@@ -1,36 +1,62 @@
+var aVal, zVal
 
-var assignment = function () {
+var FizzBuzz = (function () {
 
-  var aEl = document.getElementById("small-num");
-  var zEl = document.getElementById("big-num");
-  var aVal = aEl.value
-  var zVal = zEl.value
-  alert("you picked " + aVal + " and " + zVal +" !");
+  var brains = function(aVal, zVal){
+  var myArray =[];
   for ( aVal ; aVal <= zVal; aVal ++) {
     if (aVal % 5 === 0 && aVal % 3 === 0) {
-      var newEl = document.createElement("p");
-      var newtext = document.createTextNode("FizzBuzz");
-      newEl.appendChild(newtext);
-      document.getElementById("output").appendChild(newEl);
+      myArray.push("FizzBuzz");
+      // var newEl = document.createElement("li");
+      // var newtext = document.createTextNode("FizzBuzz");
+      // newEl.appendChild(newtext);
+      // document.getElementById("output").appendChild(newEl);
     } else if ( aVal % 5 === 0) {
-      var newEl = document.createElement("p");
-      var newtext = document.createTextNode("Buzz");
-      newEl.appendChild(newtext);
-      document.getElementById("output").appendChild(newEl);
+      myArray.push("Buzz");
+      // var newEl = document.createElement("li");
+      // var newtext = document.createTextNode("Buzz");
+      // newEl.appendChild(newtext);
+      // document.getElementById("output").appendChild(newEl);
     } else if (aVal % 3 === 0) {
-      var newEl = document.createElement("p");
-      var newtext = document.createTextNode("Fizz");
-      newEl.appendChild(newtext);
-      document.getElementById("output").appendChild(newEl);
+      myArray.push("Fizz");
+      // var newEl = document.createElement("li");
+      // var newtext = document.createTextNode("Fizz");
+      // newEl.appendChild(newtext);
+      // document.getElementById("output").appendChild(newEl);
     } else {
-      var newEl = document.createElement("p");
-      var newtext = document.createTextNode(aVal);
-      newEl.appendChild(newtext);
-      document.getElementById("output").appendChild(newEl);
+      myArray.push(aVal);
+      // var newEl = document.createElement("li");
+      // var newtext = document.createTextNode(aVal);
+      // newEl.appendChild(newtext);
+      // document.getElementById("output").appendChild(newEl);
     }
   }
-};
+    return myArray;
+  };
+
+    var _fizzbuzz = function() {};
+
+    _fizzbuzz.prototype = {
+      read: function (aVal, zVal) {
+        this.result = brains(aVal, zVal);
+      }
+    }
+  return _fizzbuzz;
+}());
+
+var myfizz = new FizzBuzz();
 
 
+document.getElementById("buttonA").addEventListener("click", function (e) {
+  e.preventDefault();
+  aVal  = document.getElementById("small-num").value;
+  zVal  = document.getElementById("big-num").value;
+  myfizz.read(aVal, zVal);
+});
+
+// var FizzBuzz = function () {
+
+//   }
+// };
 
 
